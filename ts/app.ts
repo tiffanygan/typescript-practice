48e6a4d7ac392c01678e2ac46e213147710675a2
Tiffany Gan <tiffanygan2015@gmail.com>
2020-12-05 16:45:07 -0800

    test compiler options

--- ts/app.ts
+++ ts/app.ts
@@ -1,7 +1,7 @@
 const num1 = document.getElementById('num1')! as HTMLInputElement;
 const num2 = document.getElementById('num2')! as HTMLInputElement;
-const addBtn = document.getElementById('add-btn');
-const result = document.getElementById('result');
+const addBtn = document.getElementById('add-btn')! as HTMLInputElement;
+const result = document.getElementById('result')! as HTMLInputElement;
 
 addBtn.addEventListener('click', () => {
     result.textContent = `Sum: ${parseInt(num1.value) + parseInt(num2.value)}`;
